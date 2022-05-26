bc17f22ad6ff6052377e333d952a821901a86b1b
Alekseevvv <alekseev.vladimir1212@gmail.com>
2022-05-26 15:45:39 +0300

    add menu

--- app/src/main/java/com/artava/newsapi/MainActivity.kt
+++ app/src/main/java/com/artava/newsapi/MainActivity.kt
@@ -2,35 +2,44 @@ package com.artava.newsapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.artava.newsapi.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import com.artava.newsapi.ui.FavoriteFragment
+import com.artava.newsapi.ui.SourceListFragment
+import com.artava.newsapi.ui.TimeLineFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-       /* val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_source, R.id.nav_timeline, R.id.nav_favorite
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)*/
+        loadFragment(TimeLineFragment())
+        bottomNav = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNav.setOnItemSelectedListener  { item ->
+            when (item.itemId) {
+                R.id.nav_source -> {
+                    loadFragment(SourceListFragment())
+                    true
+                }
+                R.id.nav_timeline -> {
+                    loadFragment(TimeLineFragment())
+                    true
+                }
+                R.id.nav_favor -> {
+                    loadFragment(FavoriteFragment())
+                    true
+                }
+                else -> {false}
+            }
+        }
     }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }
